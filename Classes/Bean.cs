7515d716e5bf1908cf1ffc43690d02a8f9a6e46e
Ferro Alisson (CtP/ETS) <fel9ct@bosch.com>
2024-10-29 16:21:50 -0300

    change to readonly

--- Classes/Bean.cs
+++ Classes/Bean.cs
@@ -1,9 +1,9 @@
 public record Bean(int b, int h, int yc, double gap)
 {
     public readonly int Ac = b*h; 
-    public readonly int Ieq = (b*h^3)/12;
-    public double Gap => gap;
-    public int Yc => yc;
-    public int Ytop => h/2;
-    public int Ybottom => -h/2;
+    public readonly float Ieq = b * MathF.Pow(h, 3)/12;
+    public readonly double Gap = gap;
+    public readonly int Yc = yc;
+    public readonly int Ytop = h/2;
+    public readonly int Ybottom = -h/2;
 }
